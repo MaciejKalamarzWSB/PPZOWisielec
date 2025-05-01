84c7d779303799fbf6394d6738e5c343b8c86fa0
MaciejKalamarzWSB <maciej.kalamarz@student.wsb.edu.pl>
2025-05-01 12:44:47 +0200

    Update wisielec.py

--- wisielec.py
+++ wisielec.py
@@ -1,1 +1,12 @@
-/*Gra Wisielec – klasy WordBank, Game, Player; losowanie słowa, odsłanianie liter, limit błędów, stan gry.*/
+#Gra Wisielec  klasy WordBank, Game, Player; losowanie słowa, odsłanianie liter, limit błędów, stan gry.
+
+import random
+
+with open('WordBank.txt') as file:
+    words = [line.strip() for line in file]
+
+random_word = random.choice(words)
+print("Słowo to " + random_word)
+
+for x in random_word:
+  print(x + " ")
